6f88647b3e264baf834e65b42b17a629fe5355de
yamaizumiminoru <yamaizumi.minoru@gmail.com>
2026-08-26 00:45:28 +0900

    Load browser selection import bridge

--- client-analysis.js
+++ client-analysis.js
@@ -85,6 +85,7 @@
       .then(() => loadScript("./reason-selection-client.js"))
       .then(() => loadScript("./tts-client.js"))
       .then(() => loadScript("./reason-ui-localization.js"))
+      .then(() => loadScript("./browser-import.js"))
       .catch(() => {
         // Optional enhancement: the original app remains usable if these files fail to load.
       });
