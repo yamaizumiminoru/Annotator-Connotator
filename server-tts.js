e3e9af9fc02254486f760365105d1d41067cb0ff
yamaizumiminoru <yamaizumi.minoru@gmail.com>
2026-08-26 15:06:41 +0900

    Wire context-aware question endpoint

--- server-tts.js
+++ server-tts.js
@@ -1,4 +1,6 @@
+const { installQuestionServerPatch } = require("./lib/question-server");
 const { installTtsServerPatch } = require("./lib/tts-server");
 
+installQuestionServerPatch();
 installTtsServerPatch();
 require("./server-reason-selection");
