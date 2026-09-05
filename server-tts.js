a8721f76c2c38cd6b1116dedbfccce31d8b53772
yamaizumiminoru <yamaizumi.minoru@gmail.com>
2026-09-05 13:11:59 +0900

    Install advanced recall patch explicitly

--- server-tts.js
+++ server-tts.js
@@ -1,9 +1,10 @@
 const { installQuestionServerPatch } = require("./lib/question-server");
 const { installTtsServerPatch } = require("./lib/tts-server");
+const { installAdvancedRecallPatch } = require("./server-advanced-recall");
 
 installQuestionServerPatch();
 installTtsServerPatch();
+installAdvancedRecallPatch();
 require("./server-note-policy");
 require("./server-source-formatting");
-require("./server-advanced-recall");
 require("./server-reason-selection");
